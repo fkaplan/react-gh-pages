b953cc334d3f3835295310d6d5c7260906d1d944
DESKTOP-84M71MF\furkan <furkankaplan@gmail.com>
2019-09-22 16:45:16 +0300

    Updates

---
+++ precache-manifest.2ecec69387ac34e82e2351b47fb5f1e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194610dfa83f433479b69125bde4457b",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "247523bfb78821ce2954",
+    "url": "/react-gh-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "20f717d2548a28f90648",
+    "url": "/react-gh-pages/static/js/2.9e1cd209.chunk.js"
+  },
+  {
+    "revision": "247523bfb78821ce2954",
+    "url": "/react-gh-pages/static/js/main.a2d9301a.chunk.js"
+  },
+  {
+    "revision": "1e78273ec0582e222325",
+    "url": "/react-gh-pages/static/js/runtime-main.63cd1d55.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gh-pages/static/media/logo.25bf045c.svg"
+  }
+]);
